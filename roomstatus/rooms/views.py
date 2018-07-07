5d8d920fb8fc947e3b3cdd1b33c8eb4b27717945
maishaidris <maishaidris@users.noreply.github.com>
2018-07-07 14:04:16 -0400

    Create index view

--- roomstatus/rooms/views.py
+++ roomstatus/rooms/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+# render function generates HTML files using a template & data
+from .models import Room, Ticket
 
-# Create your views here.
+
+def index(request):
+    '''View function for home page of site'''
+
+    # Generate counts of some of the main objects
+    num_rooms = Room.objects.all().count()
+    # Generate counts of total number of tickets
+    # num_tickets = Ticket.objects.all().count()
+    # Green rooms (status = 'g')
+    num_rooms_green = Rooms.objects.filter(status__exact='g').count()
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(
+        request,
+        'index.html',
+        context={'num_rooms': num_rooms, 'num_tickets': num_tickets,
+                 'num_rooms_green': num_rooms_green},
+    )
