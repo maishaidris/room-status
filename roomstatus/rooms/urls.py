3b9fa6f7f2a359c11f230aad8cd2777d8d534d0f
maishaidris <maishaidris@users.noreply.github.com>
2018-07-07 13:56:44 -0400

    Add index view to subfolder urls.py

--- roomstatus/rooms/urls.py
+++ roomstatus/rooms/urls.py
@@ -3,5 +3,7 @@ from . import views
 from django.conf.urls import include
 
 urlpatterns = [
-
+    path('', views.index, name='index'),
 ]
+# because of the name parameter, which uniquely identifies this particular URL mapping, we can now link to this home page from any other page by creating the following link in a template:
+# <a href="{% url 'index' %}">Home</a>
