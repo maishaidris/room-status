964d13a222b72a911f231bec146c71a30730f6be
maishaidris <maishaidris@users.noreply.github.com>
2018-07-11 14:54:32 -0400

    Remove cards from dashboard & set 3 per row instead of 4

---
+++ Desktop/roomproject/roomstatus/rooms/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+# render function generates HTML files using a template & data
+from .models import Room, Ticket
+from django.views import generic
+
+
+def index(request):
+    '''View function for home page of site'''
+
+    # Generate counts of some of the main objects
+    num_rooms = Room.objects.all().count()
+    # Generate counts of total number of tickets
+    num_tickets = Ticket.objects.all().count()
+    # Green rooms (status = 'g')
+    num_rooms_green = Room.objects.filter(status__exact='g').all().count()
+    # Room Names
+    rooms = Room.objects.all()
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(
+        request,
+        'index.html',
+        context={'num_rooms': num_rooms, 'num_tickets': num_tickets,
+                 'num_rooms_green': num_rooms_green, 'rooms': rooms, },
+    )
